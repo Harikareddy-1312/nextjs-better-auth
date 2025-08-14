44964d9e9f3e3dc2a93377ee17eabe0676851ef0
Harikareddy-1312 <harikareddypasunuti@gmail.com>
2025-08-14 13:34:55 +0530

    Added nextjs starter project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,102 +2,234 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full min-h-screen p-8">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="/websites/dripify"
+            className="flex flex-col items-center border border-black/10 dark:border-white/20 rounded-lg p-4 hover:shadow-lg transition"
           >
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/images/dripify.png"
+              alt="Dripify"
+              width={400}
+              height={0}
+              className="rounded-lg object-cover"
             />
-            Deploy now
+            <span className="mt-3 font-medium text-lg">Dripify</span>
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            className="flex flex-col items-center border border-black/10 dark:border-white/20 rounded-lg p-4 hover:shadow-lg transition h-full"
+
+            href="/websites/applestore"
           >
-            Read our docs
+            <Image
+              src="/images/apple.png"
+              alt="Apple"
+              width={300}
+              height={0}
+            
+              className="rounded-lg object-cover"
+            />
+            <span className="mt-3 font-medium text-lg">Apple Store</span>
+          </a>
+          <a
+             className="flex flex-col items-center border border-black/10 dark:border-white/20 rounded-lg p-4 hover:shadow-lg transition h-full"
+
+            href="/websites/cogniai"
+          >
+            <Image
+              src="/images/cogniAi.png"
+              alt="cogniAi"
+              width={300}
+              height={0}
+            
+              className="rounded-lg object-cover"
+            />
+            <span className="mt-3 font-medium text-lg">CogniAI</span>
+          </a>
+          <a
+           className="flex flex-col items-center border border-black/10 dark:border-white/20 rounded-lg p-4 hover:shadow-lg transition h-full"
+
+           
+            href="/websites/architect"
+          >
+            <Image
+              src="/images/architect.png"
+              alt="Architect"
+              width={300}
+              height={0}
+            
+              className="rounded-lg object-cover"
+            />
+            <span className="mt-3 font-medium text-lg">Architect</span>
+          </a>
+          <a
+           className="flex flex-col items-center border border-black/10 dark:border-white/20 rounded-lg p-4 hover:shadow-lg transition h-full"
+            href="/websites/clickup"
+          >
+            <Image
+              src="/images/clickup.png"
+              alt="clickup"
+              width={300}
+              height={0}
+            
+              className="rounded-lg object-cover"
+            />
+            <span className="mt-3 font-medium text-lg">Click Up</span>
+          </a>
+          <a
+            className="flex flex-col items-center border border-black/10 dark:border-white/20 rounded-lg p-4 hover:shadow-lg transition h-full"
+
+            href="/websites/freshfield"
+          >
+            <Image
+              src="/images/freshfield.png"
+              alt="freshfield"
+              width={300}
+              height={0}
+           
+              className="rounded-lg object-cover"
+            />
+            <span className="mt-3 font-medium text-lg">Freshfield</span>
+          </a>
+          <a
+            className="flex flex-col items-center border border-black/10 dark:border-white/20 rounded-lg p-4 hover:shadow-lg transition h-full" 
+            href="/websites/renewio"
+          >
+            <Image
+              src="/images/renewio.png"
+              alt="Renewio"
+              width={300}
+              height={0}
+             
+              className="rounded-lg object-cover"
+            />
+            <span className="mt-3 font-medium text-lg">Renewio</span>
+          </a>
+          <a
+            className="flex flex-col items-center border border-black/10 dark:border-white/20 rounded-lg p-4 hover:shadow-lg transition h-full"
+            href="/websites/health"
+          ><Image
+              src="/images/health.png"
+              alt="Health"
+              width={300}
+              height={0}
+            
+              className="rounded-lg object-cover"
+            />
+            <span className="mt-3 font-medium text-lg">Health</span>
+          </a>
+          <a
+            className="flex flex-col items-center border border-black/10 dark:border-white/20 rounded-lg p-4 hover:shadow-lg transition h-full"
+            href="/websites/rebuild"
+          >
+            <Image
+              src="/images/rebuild.png"
+              alt="Rebuild"
+              width={300}
+              height={0}
+             
+              className="rounded-lg object-cover"
+            />
+            <span className="mt-3 font-medium text-lg">Rebuild</span>
+          </a>
+          <a
+            className="flex flex-col items-center border border-black/10 dark:border-white/20 rounded-lg p-4 hover:shadow-lg transition h-full"
+            href="/websites/ecofins"
+          ><Image
+              src="/images/ecofins.png"
+              alt="Ecofins"
+              width={300}
+              height={0}
+             
+              className="rounded-lg object-cover"
+            />
+            <span className="mt-3 font-medium text-lg">Ecofins</span>
+          </a>
+          <a
+            className="flex flex-col items-center border border-black/10 dark:border-white/20 rounded-lg p-4 hover:shadow-lg transition h-full"
+            href="/websites/passionfroot"
+          >
+            <Image
+              src="/images/passionfroot.png"
+              alt="Passionfroot"
+              width={300}
+              height={0}
+             
+            
+              className="rounded-lg object-cover"
+            />
+            <span className="mt-3 font-medium text-lg">PassionFroot</span>
+          </a>
+          <a
+            className="flex flex-col items-center border border-black/10 dark:border-white/20 rounded-lg p-4 hover:shadow-lg transition h-full"
+            href="/websites/wavespace"
+          >
+            <Image
+              src="/images/wavespace.png"
+              alt="Wavespace"
+              width={300}
+              height={0}
+              className="rounded-lg object-cover"
+            />
+            <span className="mt-3 font-medium text-lg">WaveSpace</span>
+          </a>
+          <a
+            className="flex flex-col items-center border border-black/10 dark:border-white/20 rounded-lg p-4 hover:shadow-lg transition h-full"
+            href="/websites/ebook"
+          >
+            <Image
+              src="/images/eBook.png"
+              alt="eBook"
+              width={300}
+              height={0}
+              className="rounded-lg object-cover"
+            />
+            <span className="mt-3 font-medium text-lg">eBook</span>
+          </a>
+          <a
+            className="flex flex-col items-center border border-black/10 dark:border-white/20 rounded-lg p-4 hover:shadow-lg transition h-full"
+            href="/websites/lyniq"
+          >
+            <Image
+              src="/images/lyniq.png"
+              alt="Lyniq"
+              width={300}
+              height={0}
+              className="rounded-lg object-cover"
+            />
+            <span className="mt-3 font-medium text-lg">Lyniq</span>
+          </a>
+          <a
+            className="flex flex-col items-center border border-black/10 dark:border-white/20 rounded-lg p-4 hover:shadow-lg transition h-full"
+            href="/websites/unifi"
+          >
+            <Image
+              src="/images/unifi.png"
+              alt="Unifi"
+              width={300}
+              height={0}
+              className="rounded-lg object-cover"
+            />
+            <span className="mt-3 font-medium text-lg">Unifi</span>
+          </a>
+          <a
+            className="flex flex-col items-center border border-black/10 dark:border-white/20 rounded-lg p-4 hover:shadow-lg transition h-full"
+            href="/websites/jeton"
+          >
+            <Image
+              src="/images/jeton.png"
+              alt="Jeton"
+              width={300}
+              height={0}
+              className="rounded-lg object-cover"
+            />
+            <span className="mt-3 font-medium text-lg">Jeton</span>
           </a>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
+
